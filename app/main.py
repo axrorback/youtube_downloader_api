891e7f98f62f05f75b6b1c6aa95a1805b40fc2fe
axrorback <axrorback@gmail.com>
2025-09-04 15:07:26 +0500

    add root endpoint

--- app/main.py
+++ app/main.py
@@ -6,6 +6,9 @@ app = FastAPI(
     description="🎥 YouTube videolarini va audiolarini yuklab olish uchun API",
     version="1.0.0",
 )
+@app.get("/")
+def root():
+    return {"message": "YouTube Downloader API ishlayapti 🚀"}
 
 # Routes
 app.include_router(youtube.router, prefix="/youtube", tags=["YouTube"])
